d65cc8694c35af038b4a7e816cbb37698963a4ba
Andrea Amorosi <dreamorosi@gmail.com>
2023-04-01 16:58:22 +0000

    feat: update to cdk v2, simplify policies, switch to vite

--- amplify/backend/awscloudformation/override.ts
+++ amplify/backend/awscloudformation/override.ts
@@ -1,59 +1,52 @@
-import { AmplifyRootStackTemplate } from "@aws-amplify/cli-extensibility-helper";
+import {
+  AmplifyProjectInfo,
+  AmplifyRootStackTemplate,
+} from "@aws-amplify/cli-extensibility-helper";
 
-export function override(resources: AmplifyRootStackTemplate) {
-  // Give permissions to perform the rum:PutEvents action to the authenticated role
-  // & change the name so we can reference it later
+export function override(
+  resources: AmplifyRootStackTemplate,
+  amplifyProjectInfo: AmplifyProjectInfo
+) {
+  // Policy that allows an IAM role to perform the rum:PutEvents action on the CloudWatch RUM API
+  const policy = {
+    policyName: "cloudwatch-rum-put-events",
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: "Allow",
+          // AWS Region and Account ID (https://docs.aws.amazon.com/IAM/latest/UserGuide/console_account-alias.html)
+          // and your Amplify project info are replaced dynamically.
+          Resource: {
+            "Fn::Sub":
+              "arn:aws:rum:${AWS::Region}:${AWS::AccountId}:appmonitor/app-monitor-" +
+              `${amplifyProjectInfo.projectName}-${amplifyProjectInfo.envName}`,
+          },
+          Action: ["rum:PutRumEvents"],
+        },
+      ],
+    },
+  };
+
+  // Rename the auth & unauth roles so we can reference them later
   const authRole = resources.authRole;
   authRole.roleName = "amplify-amplifycloudwatchrum-authRole";
+  const unauthRole = resources.unauthRole;
+  unauthRole.roleName = "amplify-amplifycloudwatchrum-unauthRole";
+
+  // Store the base policies so we can add our own
   const authRoleBasePolicies = Array.isArray(authRole.policies)
     ? authRole.policies
     : [authRole.policies];
 
-  authRole.policies = [
-    ...authRoleBasePolicies,
-    {
-      policyName: "cloudwatch-rum-put-events",
-      policyDocument: {
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Effect: "Allow",
-            // Replace AWS Region and Account ID (https://docs.aws.amazon.com/IAM/latest/UserGuide/console_account-alias.html)
-            // of your Amplify project, as well as the correct project’s name and environment.
-            Resource:
-              "arn:aws:rum:[aws-region]:[aws-account-id]:appmonitor/app-monitor-[amplify-project-name]-[env]",
-            Action: ["rum:PutRumEvents"],
-          },
-        ],
-      },
-    },
-  ];
+  // Give permissions to perform the rum:PutEvents action to the authenticated role
+  authRole.policies = [...authRoleBasePolicies, policy];
 
-  // Give permissions to perform the rum:PutEvents action to the unauthenticated role
-  // & change the name so we can reference it later
-  const unauthRole = resources.unauthRole;
-  unauthRole.roleName = "amplify-amplifycloudwatchrum-unauthRole";
+  // Store the base policies so we can add our own
   const unauthRoleBasePolicies = Array.isArray(unauthRole.policies)
     ? unauthRole.policies
     : [unauthRole.policies];
 
-  unauthRole.policies = [
-    ...unauthRoleBasePolicies,
-    {
-      policyName: "cloudwatch-rum-put-events",
-      policyDocument: {
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Effect: "Allow",
-            // Replace AWS Region and Account ID (https://docs.aws.amazon.com/IAM/latest/UserGuide/console_account-alias.html)
-            // of your Amplify project, as well as the correct project’s name and environment.
-            Resource:
-              "arn:aws:rum:[aws-region]:[aws-account-id]:appmonitor/app-monitor-[amplify-project-name]-[env]",
-            Action: ["rum:PutRumEvents"],
-          },
-        ],
-      },
-    },
-  ];
+  // Give permissions to perform the rum:PutEvents action to the unauthenticated role
+  unauthRole.policies = [...unauthRoleBasePolicies, policy];
 }
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,29 +1,30 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "amplifycloudwatchrumauth": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "function": {
-        "cfncustomfn": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
-    "custom": {
-        "cloudwatchrum": {
-            "AppMonitorName": "string",
-            "AppMonitorId": "string",
-            "GuestRoleArn": "string",
-            "IdentityPoolId": "string"
-        }
+  "auth": {
+    "amplifycloudwatchrumauth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
+  },
+  "custom": {
+    "cloudwatchrum": {
+      "AppMonitorId": "string",
+      "AppMonitorName": "string",
+      "GuestRoleArn": "string",
+      "IdentityPoolId": "string"
+    }
+  },
+  "function": {
+    "cfncustomfn": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
 }
